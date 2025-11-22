2a41a3f75f3678019e647af20a8865fca0b4e7a0
hary <harydpoudel@gmail.com>
2025-11-22 11:38:36 +0000

    Add project files.

---
+++ mbbs_testing/Models/QuestionItem.cs
@@ -0,0 +1,16 @@
+﻿namespace mbbs_testing.Models
+{
+    public class QuestionItem
+    {
+        public string? question { get; set; }
+        public string? opa { get; set; }
+        public string? opb { get; set; }
+        public string? opc { get; set; }
+        public string? opd { get; set; }
+        public string? subject_name { get; set; }
+        public string? topic_name { get; set; }
+        public string? id { get; set; }
+        public string? choice_type { get; set; }
+        //public string? explanation { get; set; }
+    }
+}
